954409d5a83d759aa2c9a1bef566f1de1885649d
uitkeps <phamthanhnhut166@gmail.com>
2022-11-17 23:06:34 +0700

    update 5

---
+++ Ex 1.7/src/connect.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+const messages = [
+    {
+        author: 'SC',
+        text: 'Rolls complete and a pitch is program. One BRAVO.'
+    },
+    {
+        author: 'zPAO',
+        text: 'One BRAVO is an abort control model. Attitude is 2 miles',
+    },
+    {
+        author: 'CAPCOM',
+        text: 'All is well at Houston. You are good at 1 minute.',
+    },
+]
+
+app.get('/messages/:forum', (request, response) => {
+    if (request.params && request.params.forum === 'nasa') {
+        return response.send(messages)
+    }
+    return response.status(404).send({error:'Unknown forum'})
+})
+app.post('/messages/:forum',(request, response) => {
+    if (request.params && request.params.forum === 'nasa') {
+        messages.push(request.body)
+        return response.send('OK')
+    }
+    return response.status(404).send({error: 'Unknown forum'})
+})
