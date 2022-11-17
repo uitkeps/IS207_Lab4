5782811542885699bec6dba156f3f21ebfe9c096
uitkeps <phamthanhnhut166@gmail.com>
2022-11-17 22:43:03 +0700

    update 2

---
+++ Ex 1.2/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import * as React from "react";
+import { FadeLoader } from "react-spinners";
+import MyPage from "./MyPage";
+
+function App() {
+  return (
+    <React.Suspense fallback={<FadeLoader color={"lightblue"} size={150} />}>
+      <MyPage />
+    </React.Suspense>
+  );
+}
+
+export default App;
